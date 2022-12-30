16c58b91e53fc6f0e8f343980ef74d0ffd978893
Coco8572 <99449338+Coco8572@users.noreply.github.com>
2022-12-29 17:08:40 -0700

    created navbar & fixed color error in setup & config

--- bookmark/tailwind.config.js
+++ bookmark/tailwind.config.js
@@ -10,10 +10,10 @@ module.exports = {
         },
         extend: {
             colors: {
-                softBlue: "hsl(231, 69%, 60%",
-                softRed: "hsl(0, 94%, 66%",
-                softBlue: "hsl(229, 8%, 60%",
-                softBlue: "hsl(229, 31%, 21%",
+                softBlue: "hsl(231, 69%, 60%)",
+                softRed: "hsl(0, 94%, 66%)",
+                grayishBlue: "hsl(229, 8%, 60%)",
+                veryDarkBlue: "hsl(229, 31%, 21%)",
             },
             fontFamily: {
                 sans: ["Rubik", "sans-serif"],
